bbac4f63347cfcf8c349bf63f007c395ae40cf49
Obi Wan Jabronii <mitchell_lough@yahoo.com>
2022-07-14 15:41:29 -0400

    created candidate table in election db and seeded said table

---
+++ db/seeds.sql
@@ -0,0 +1,9 @@
+INSERT INTO candidates (first_name, last_name, industry_connected)
+VALUES 
+    ('Ronald', 'Mcdonald', 1),
+    ('Taco', 'Bell', 1),
+    ('Jack', 'IntheBox', 0),
+    ('Big', 'Boy', 0),
+    ('Burger', 'King', 1),
+    ('White', 'Castle', 0),
+    ('Carls', 'Jr', 1);
