b49aa1e22048d24b9717a601a87a438690e615f6
Obi Wan Jabronii <mitchell_lough@yahoo.com>
2022-07-18 12:50:07 -0400

    added a parties table and seeded said table

--- db/seeds.sql
+++ db/seeds.sql
@@ -7,3 +7,9 @@ VALUES
     ('Burger', 'King', 1),
     ('White', 'Castle', 0),
     ('Carls', 'Jr', 1);
+
+INSERT INTO parties (name, description)
+VALUES 
+    ('JavaScripters for Tomorrow', 'These dudes can barely function without making eveything about JavaScript. Seriously though its a problem.'),
+    ('HTML Heratics', 'A party of the most basic kind of people. There are Uggs and pumpkin spice lattes everywhere.'),
+    ('Gidiots', 'By far the most reasonable of the parties. Unless there is a merge conflict, then they will become incredibly difficult and entirely unreasonable.');
