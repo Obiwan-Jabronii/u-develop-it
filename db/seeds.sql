66fb9d68544ecd6c3e144c195c12316aab65fb3d
Obi Wan Jabronii <mitchell_lough@yahoo.com>
2022-07-18 13:25:37 -0400

    added fk to candidates and updated the express endpoint to add the party affiliation

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,15 +1,16 @@
-INSERT INTO candidates (first_name, last_name, industry_connected)
-VALUES 
-    ('Ronald', 'Mcdonald', 1),
-    ('Taco', 'Bell', 1),
-    ('Jack', 'IntheBox', 0),
-    ('Big', 'Boy', 0),
-    ('Burger', 'King', 1),
-    ('White', 'Castle', 0),
-    ('Carls', 'Jr', 1);
-
 INSERT INTO parties (name, description)
 VALUES 
     ('JavaScripters for Tomorrow', 'These dudes can barely function without making eveything about JavaScript. Seriously though its a problem.'),
     ('HTML Heratics', 'A party of the most basic kind of people. There are Uggs and pumpkin spice lattes everywhere.'),
     ('Gidiots', 'By far the most reasonable of the parties. Unless there is a merge conflict, then they will become incredibly difficult and entirely unreasonable.');
+
+INSERT INTO candidates (first_name, last_name, industry_connected, party_id)
+VALUES 
+    ('Ronald', 'Mcdonald', 1, 1),
+    ('Taco', 'Bell', 1, 3),
+    ('Jack', 'IntheBox', 0, 2),
+    ('Big', 'Boy', 0, NULL),
+    ('Burger', 'King', 1, 2),
+    ('White', 'Castle', 0, 1),
+    ('Carls', 'Jr', 1, 3);
+
